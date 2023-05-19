a62822ff2acadcfc8d68a627a462452e6e5f0566
Santiago Hernández <69361127+santihg13@users.noreply.github.com>
2023-05-19 17:29:01 -0600

    Add files via upload

--- models.py
+++ models.py
@@ -8,3 +8,11 @@ class Venta(models.Model):
 
     def __str__(self):
         return f"Venta de {self.monto_venta} en la tienda {self.id_tienda}"
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
