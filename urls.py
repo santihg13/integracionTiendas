482535e089fa9dd97fddf9a1739cfa8d79a676c3
Santiago Hernández <69361127+santihg13@users.noreply.github.com>
2023-05-19 17:24:32 -0600

    Carga de archivos de trabajo
    No deja subir carpetas; faltan el resto de archivos de Django

---
+++ urls.py
@@ -0,0 +1,44 @@
+"""
+URL configuration for integracionTiendas project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, register_converter
+from . import views
+from soapbox import soap_dispatch
+
+
+class FloatConverter:
+    regex = '[0-9]+(\.[0-9]+)?'
+
+    def to_python(self, value):
+        return float(value)
+
+    def to_url(self, value):
+        return str(value)
+
+register_converter(FloatConverter, 'float')
+
+class RegistroVentaService:
+    @soap_dispatch.bind('RegistrarVenta')
+    def registrar_venta(self, nombre_aplicacion, id_tienda, monto_venta, nombre_vista):
+        # Lógica para registrar la venta
+        return "Venta registrada correctamente"
+
+urlpatterns = [
+    path('ventas/<str:nombre_aplicacion>/<int:id_tienda>/<float:monto_venta>/<str:nombre_vista>/', views.registrar_venta),
+    path('ventas/soap/', RegistroVentaService.registrar_venta, name='registrar_venta_soap'),
+    path('my-view/', views.my_view, name='my-view'),
+]
---
+++ views.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import requests
+import zeep
+
+@csrf_exempt
+def registrar_venta(request, nombre_aplicacion, id_tienda, monto_venta, nombre_vista):
+    if id_tienda == 1:
+        # SOAP
+        wsdl_url = 'http://ejemplo.com/soap/service?wsdl'
+        client = zeep.Client(wsdl=wsdl_url)
+
+        try:
+            response = client.service.RegistrarVenta(nombre_aplicacion, id_tienda, monto_venta, nombre_vista)
+            return HttpResponse("Venta registrada correctamente")
+        except Exception as e:
+            return HttpResponse("Error al registrar la venta: " + str(e))
+
+    else:
+        # RESTful
+        api_url = 'http://ejemplo.com/api/v1/ventas'
+
+        try:
+            response = requests.post(api_url, json={'nombre_aplicacion': nombre_aplicacion,
+                                                    'id_tienda': id_tienda,
+                                                    'monto_venta': monto_venta,
+                                                    'nombre_vista': nombre_vista})
+
+            if response.status_code == 201:
+                return HttpResponse("Venta registrada correctamente")
+            else:
+                return HttpResponse("Error al registrar la venta")
+        except requests.exceptions.RequestException as e:
+            return HttpResponse("Error al comunicarse con la API: " + str(e))
+
+from django.http import JsonResponse
+
+def my_view(request):
+    # Realiza la conexión al servicio RESTful
+    response = requests.get('http://ejemplo.com/api/endpoint')
+
+    if response.status_code == 200:
+        # La solicitud fue exitosa
+        data = response.json()
+        # Procesa los datos recibidos
+        return JsonResponse(data)  # Devuelve los datos como una respuesta JSON
+    else:
+        # Ocurrió un error en la solicitud
+        error_message = response.text
+        # Maneja el error según tus necesidades
+        return JsonResponse({'error': error_message}, status=response.status_code)  # Devuelve un mensaje de error en JSON
